334cee47cb3212d72493b4c9f907a0acc9965383
Ivan Pyatanin <ivan.pyatanin@yandex.ru>
2023-09-23 03:26:04 +0300

    Second commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ connection = psycopg2.connect(database="clients_db", user="postgres", password="
 def drop_tables():
     with connection.cursor() as cur:
         cur.execute("""drop table client, phone;""")
+    print("Tables have been deleted")
 
 #функция, создающая БД
 def create_db():
@@ -44,9 +45,23 @@ def create_phone(phones, client_id):
 
 #Изменение инфыормации
 def update_client(client_id, name=None, first_name=None, email=None):
-    with connection.cursor() as cur:
-        cur.execute("""UPDATE client set name = %s, first_name = %s, email = %s where id = %s""", (name, first_name, email, client_id,))
-    print("Данные обновлены")
+    if name != "":
+        with connection.cursor() as cur:
+            cur.execute("""
+            update client set name = %s where id = %s""", (name, client_id))
+        print("Name updated")
+    elif first_name != "":
+        with connection.cursor() as cur:
+            cur.execute("""
+            update client set first_name = %s where id = %s""", (first_name, client_id))
+        print("First name updated")
+    elif email != "":
+        with connection.cursor() as cur:
+            cur.execute("""
+            update client set email = %s where id = %s""", (email, client_id))
+        print("Email updated")
+    else:
+        print("erorr")
 
 #удаление телефона
 def delete_phone(phones, client_id):
@@ -58,7 +73,8 @@ def delete_phone(phones, client_id):
 def delete_client(client_id):
     with connection.cursor() as cur:
         cur.execute("""
-        delete from client where id = %s""", (client_id,))
+            delete from phone
+                where client_id in (select id from client where id = %s)""", (client_id,))
     print(f'Клиент под id {client_id} удален')
 
 #поиск клиента
@@ -84,14 +100,14 @@ def find_client(name=None, first_name=None, email=None, phones=None):
 # create_client("Ivan", "Pyatanin", "ivan.pyatanin@yandex.ru",)
 # create_client("Oleg", "Murkin", "Murk@mail.ru",)
 
-# delete_client(3)
+# delete_client(1)
 
 # create_phone("+79999999999", 1)
 # create_phone("+79998989988", 2)
 
-# update_client(1,"Ivan",)
+# update_client(1, '', 'Minin')
 
-# find_client('', 'Pyatanin')
+# find_client('Ivan',)
 # delete_phone("+79999999999", 1)
 
 
